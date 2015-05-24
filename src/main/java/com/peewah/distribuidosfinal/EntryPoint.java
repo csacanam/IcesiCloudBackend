0b229b668dbe086b21c6bd9e5444cb256c877a2d
csacanam <csacanam@gmail.com>
2015-05-24 15:37:50 -0500

    Se remueven imports innecesarios

--- src/main/java/com/peewah/distribuidosfinal/EntryPoint.java
+++ src/main/java/com/peewah/distribuidosfinal/EntryPoint.java
@@ -35,8 +35,6 @@ import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 import org.apache.commons.io.FileUtils;
 import spark.Request;
 import spark.Response;
